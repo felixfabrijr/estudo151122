8576588c884db6c6f08f35f6d31526f73cf259aa
44789 <44789@LAPTOP-15BHDUNE>
2022-11-15 20:04:25 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace TrabStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var paragrafo = "     C# linguagem moderna e versatil." +
+                    "C# desenvole web, desktop, jogos, " +
+                    "Mobile, entre outros. ";
+
+            // Length
+            var tamanho = paragrafo.Length;
+
+            // Empty
+            var vazio = string.Empty;
+
+            // ToLower, ToUpper
+            var paragrafoMinuscula = paragrafo.ToLower();
+            var paragrafoMaiusculo = paragrafo.ToUpper();
+
+            // Split
+            var frases = paragrafo.Split('.');
+
+            // Trim, TrimEnd, TrimStart
+            var paragrafoTrim = paragrafo.Trim();
+            var paragrafoTrimEnd = paragrafo.TrimEnd();
+            var paragrafoTrimStart = paragrafo.TrimStart();
+
+            // IsNullOrWhitespace
+            var isNullOrwhiteSpace = string.IsNullOrWhiteSpace(paragrafo);
+
+            // Replace
+            var paragrafoCsharp = paragrafo.Replace("C#", "C-sharp");
+
+            Console.ReadKey();
+
+        }
+    }
+}
